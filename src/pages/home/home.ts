5d04c0e9b31a48fcea2e5a0f9afdde535f5c0641
Paul <wenzel.pgb@googlemail.com>
2017-12-08 18:10:29 +0100

    added input field to the home-page for the configuration

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {SensorsPage} from "../sensors/sensors";
+import {Logger} from "../../app/logger";
 
 @Component({
   selector: 'page-home',
@@ -12,8 +13,11 @@ export class HomePage {
 
   }
 
-  private openSensorPage() {
-    this.navCtrl.push(SensorsPage);
+  private openSensorPage(organisation: string, deviceType: string, deviceId: string, authenticationToken: string) {
+    // store the configuration data to the local app storage
+
+    // call the sensor page to start the tracking
+//    this.navCtrl.push(SensorsPage);
   }
 
 }
